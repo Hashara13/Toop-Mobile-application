034336e375af814bccc0e36c6a3b5996c7dffa0b
Hashara13 <hnethmin13@gmail.com>
2024-06-26 16:19:05 +0530

    pricing mapped

--- components/Pricees.js
+++ components/Pricees.js
@@ -1,43 +1,109 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import SwitchSelector from 'react-native-switch-selector';
+import React, { useState } from "react";
+import { View, Text, StyleSheet,ScrollView } from "react-native";
+import SwitchSelector from "react-native-switch-selector";
+import COLORS from "../constants/colors";
+import { useFonts } from "expo-font";
+import Button from "./Button";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { priceList } from "../constants/pricingPack";
 
 const Prices = () => {
-  const [selectedPackage, setSelectedPackage] = useState('standard');
+  const [fontsLoaded] = useFonts({
+    "CustomFont-Bold": require("../assets/fonts/static/DMSans-Bold.ttf"),
+  });
+
+  const [selectedPackage, setSelectedPackage] = useState("standard");
 
   const options = [
-    { label: "Standard", value: "standard" },
-    { label: "Basic", value: "basic" }
+    { label: "Annual Access", value: "Annual Access" },
+    { label: "Per Track", value: "Per Track" },
   ];
 
   return (
     <View style={styles.container}>
-      <SwitchSelector
-        options={options}
-        initial={0}
-        onPress={value => setSelectedPackage(value)}
-        buttonColor="#f5dd4b"
-        backgroundColor="#81b0ff"
-        textColor="#f4f3f4"
-        selectedColor="#fff"
-        borderColor="#767577"
-        hasPadding
-      />
+      <View style={styles.TextContainer}>
+        <Text style={styles.HeaderText}>Select Pricing Plan</Text>
+        <View style={styles.ButtonContainer}>
+          <SwitchSelector
+            options={options}
+            initial={0}
+            onPress={(value) => setSelectedPackage(value)}
+            style={styles.switchSelector}
+            buttonColor="#424141"
+            backgroundColor="#FFFFFF"
+            textColor="#222222"
+            selectedColor="#222222"
+            borderColor="#222222"
+            hasPadding
+            height={40}
+            width={300}
+            borderRadius={20}
+            fontSize={16}
+            fontWeight="800"
+            shadowColor="#222222"
+            shadowOffset={{ width: 0, height: 2 }}
+            shadowOpacity={0.3}
+            shadowRadius={20}
+            elevation={5}
+            optionStyle={{ margin: 3 }}
+            selectedTextStyle={{ color: "white" }}
+            unselectedTextStyle={{ color: "blue" }}
+          />{" "}
+        </View>{" "}
+      </View>
       <View style={styles.packageContainer}>
-        {selectedPackage === 'standard' ? (
-          <View style={styles.packageDetails}>
-            <Text style={styles.packageTitle}>Standard Package</Text>
-            <Text style={styles.packagePrice}>Price: $50/month</Text>
-            <Text style={styles.packageDescription}>
-              The Standard Package includes more features and higher royalties.
-            </Text>
+     
+        {selectedPackage === "Annual Access" ? (
+          <View style={styles.packageBase}>
+             {/* <ScrollView horizontal showsHorizontalScrollIndicator={false}> */}
+            {priceList.map(
+              ({ id, Title, Price, Social, AudioD, Digital, Dev, Events }) => (
+                <View style={styles.packageDetails} key={id}>
+                  <Text style={styles.packageTitle}>{Title}</Text>
+                  <Text style={styles.packagePrice}>{Price}</Text>
+                  <View style={styles.packageIcon}>
+                    <MaterialCommunityIcons
+                      name="emoticon-cool"
+                      size={24}
+                      color="black"
+                    />
+                    <Text style={styles.packageDescription}>
+                      {Social}
+                    </Text>
+                  </View>
+                  <Button
+                    style={{
+                      flex: 1,
+                      paddingBottom: 10,
+                      paddingVertical: 7,
+                      paddingHorizontal: 7,
+                      width: "50%",
+                      borderColor: COLORS.lightblue,
+                      backgroundColor: COLORS.lightblue,
+                      marginVertical: 10,
+                      marginHorizontal: 0,
+                      borderWidth: 1,
+                      borderRadius: 12,
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                    fontSize={16}
+                    fontWeight={600}
+                    color={COLORS.white}
+                    title="PURCHASE"
+                  />
+                </View>
+              )
+            )}
+          {/* </ScrollView> */}
           </View>
         ) : (
           <View style={styles.packageDetails}>
             <Text style={styles.packageTitle}>Basic Package</Text>
             <Text style={styles.packagePrice}>Price: $30/month</Text>
             <Text style={styles.packageDescription}>
-              The Basic Package includes essential features with standard royalties.
+              The Basic Package includes essential features with standard
+              royalties.
             </Text>
           </View>
         )}
@@ -49,28 +115,58 @@ const Prices = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
+    marginHorizontal:10,
+    justifyContent: "center",
+    alignItems: "center",
+    // padding: 16,
+  },
+  packageBase: {
+   
+    width: "80%",
+    paddingVertical: 5,
+    paddingHorizontal:10,
+    marginVertical:10,
+    marginHorizontal:10,
+
+    marginHorizontal: 12,
+    flexDirection: "column",
   },
   packageContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 20,
+    marginHorizontal:10,
+  },
+  packageIcon: {
+    flexDirection: "row",
   },
   packageDetails: {
-    alignItems: 'center',
+    alignItems: "center",
     padding: 20,
+    paddingVertical: 10,
+                paddingHorizontal: 10,
+                marginHorizontal: 10,
+    backgroundColor: COLORS.white,
     borderRadius: 10,
-    backgroundColor: '#f0f0f0',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
+    marginVertical: 10,
+    fontFamily: "CustomFont-Bold",
+    borderWidth: 1,
+    borderColor: COLORS.grey2,
+    shadowColor: COLORS.black,
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.1,
+    shadowRadius: 7,
     elevation: 5,
+  },
+  switchSelector: {
+    shadowColor: "#222222",
+    // // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.1,
+    // shadowRadius: 20,
+    // // elevation: 5,
   },
   packageTitle: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   packagePrice: {
@@ -79,7 +175,30 @@ const styles = StyleSheet.create({
   },
   packageDescription: {
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
+  },
+  TextContainer: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    marginHorizontal: 10,
+    //  alignItems:'center',
+    alignContent: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    marginTop: 2,
+    width: "80%",
+  },
+  HeaderText: {
+    fontSize: 18,
+    color: COLORS.primary,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 8,
+    fontWeight: 600,
+    textAlign: "center",
+    paddingTop: 8,
+    paddingHorizontal: 10,
+    fontFamily: "CustomFont-Bold",
   },
 });
 
