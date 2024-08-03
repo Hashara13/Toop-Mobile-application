8f6faf8932cfa61324915662560b5a34fb8d0ba6
Hashara13 <hnethmin13@gmail.com>
2024-08-03 11:37:21 +0530

    music model added

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const toopMoel=require("./models/toop");
+const multer = require('multer');   
+const toopModel=require("./models/toop");
+const uploadModel=require("./models/uploadModel")
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  });
+const upload = multer({ storage: storage });
+
 
 const app = express()
 app.use(express.json());
@@ -11,7 +24,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/Toop_Application");
 
 app.post("/login",(req,res)=>{
 const {email,password}=req.body;
-toopMoel.findOne({email:email})
+toopModel.findOne({email:email})
 .then(user=>{
     if(user){
         if(user.password===password){
@@ -32,10 +45,14 @@ app.post("/register",async(req,res)=>{
     .catch(err=>res.json(err))
 })
 
-app.post("/upload",async(req,res)=>{
-    uploadMoel.create(req.body)
-    .then(uphere=>res.json(uphere))
-    .catch(err=>res.json(err))
+app.post("/upload", upload.single('file'), async (req, res) => {
+    try{
+        const file=req.file;
+        const{title,musicCategory,contributorName,contributorPercentage,contributorRole,packageName,packagerPrice,commercialUses,events}=req.body;
+        const newMusic=new uploadModel({
+            title:title||file.title,
+        })
+    }
 }
 )
 
--- Backend/models/uploadModel.js
+++ Backend/models/uploadModel.js
@@ -1,10 +1,40 @@
 const mongoose = require("mongoose");
 const uploadSchema=new mongoose.Schema({
-    username:String,
-    email:{type:String,unique:true},
-    mobile:String,
-    password:String,
-})
-
-const uploadMoel=mongoose.model("toopapp",uploadSchema)
-module.exports=uploadMoel
+    title: {
+        type: String,
+        required: true,
+      },
+      musicCategory: {
+        required: true,
+      },
+      contributorName: {
+        type: String,
+        required: true,
+      },
+      contributorPercentage: {
+        type: Number,
+        required: true,
+      },
+      contributorRole: {
+        type: Number,
+        required: true,
+      },
+      packageName: {
+        type: String,
+        required: true,
+      },
+      packagerPrice: {
+        type: Number,
+        required: true,
+      },
+      commercialUses: {
+        type: Number,
+        required: true,
+      },
+      events: {
+        type: Number,
+        required: true,
+      },
+    });
+const uploadModel=mongoose.model("toopapp",uploadSchema)
+module.exports=uploadModel
